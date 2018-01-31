e26c009b2ec80422047cead2d2b5590d1ef6adb6
ji <nfi@gmail.com>
2018-02-01 11:59:20 +1300

    2nd Thurs morning - move to react

---
+++ client/components/Questions.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Questions = () => {
+  const question = this.props.question
+  var answer = this.props.answer
+  render() {
+    <div class = 'container'></div>
+      <form method = 'post'>
+      <input name="answer" type="radio" class="answerButton" value='A'> </input>
+      <input type="submit"> </input>
+</form>
+</div>
+  }
+}
--- routes.js
+++ routes.js
@@ -8,36 +8,36 @@ var router = express.Router()
 //   res.render('home')
 // })
 
-router.get('/quiz', function (req, res) {
-  //
-  // var newId=20002
-  var db = req.app.get('db')
+// router.get('/quiz', function (req, res) {
+//   //
+//   // var newId=20002
+//   var db = req.app.get('db')
 
-    db('80s_qs')
-      .join('80s_answs', '80s_qs.id', '80s_answs.80s_q_id')
-      .where('80s_q_id', 20002)
-      .then((data) => {
-        res.render('quiz', {question1: data[0], data})
-      })
+//     db('80s_qs')
+//       .join('80s_answs', '80s_qs.id', '80s_answs.80s_q_id')
+//       .where('80s_q_id', 20002)
+//       .then((data) => {
+//         res.render('quiz', {question1: data[0], data})
+//       })
 
-})
+// })
 
-router.post('/quiz', function (req, res) {
-  var answerChosen = req.body.answer
-  var db = req.app.get('db')
+// router.post('/quiz', function (req, res) {
+//   var answerChosen = req.body.answer
+//   var db = req.app.get('db')
 
-  if (answerChosen.isCorrect) {
-   score+=5
-  } else score-=2
+//   if (answerChosen.isCorrect) {
+//    score+=5
+//   } else score-=2
 
-  loadnextquestion
+//   loadnextquestion
 
-})
+// })
 
 
 //join the two quiz tables so user can answer
 
-function findCorrectAnswer
+// function findCorrectAnswer
 // db.(select_cham_q)
 //   .where(isCorrect == true)
 //   .then ((data) => {
@@ -47,7 +47,7 @@ function findCorrectAnswer
 //find specific question
 
 
-function (select_cham_)
+// function (select_cham_)
 // db('cham_qs')
 // .join('cham_answs', 'cham_qs.id', 'cham_answs.cham_q_id')
 //   .select('cham_answs', 'cham_qs', 'cham_answs.isCorrect' )
