d4ef9d67d38acc5b9c59a2f0c1c6b523e5424f37
James Bryant <JB@James-Bryants-MacBook-Pro.local>
2018-02-11 15:45:53 +1300

    updated seeds

--- seeds/scores.js
+++ seeds/scores.js
@@ -10,6 +10,11 @@ exports.seed = function(knex, Promise) {
         knex('scores').insert({id: 10013, user_id: 10003, scores: 50, username: 'Mel'}),
         knex('scores').insert({id: 10014, user_id: 10004, scores: 20, username: 'Maia'}),
         knex('scores').insert({id: 10015, user_id: 10005, scores: 10, username: 'Guy'}),
+        knex('scores').insert({id: 10016, user_id: 10006, scores: 40, username: 'Phuong'}),
+        knex('scores').insert({id: 10017, user_id: 10007, scores: 30, username: 'Nick'}),
+        knex('scores').insert({id: 10018, user_id: 10008, scores: 50, username: 'Jess'}),
+        knex('scores').insert({id: 10019, user_id: 10009, scores: 20, username: 'Hayden'}),
+        knex('scores').insert({id: 10020, user_id: 10010, scores: 10, username: 'James'}),
       ])
     })
 }
