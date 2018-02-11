2408475ad776ed6d78cbd400d21e29535959975e
James Bryant <JB@James-Bryants-MacBook-Pro.local>
2018-02-11 14:04:12 +1300

    update datatbase

--- seeds/scores.js
+++ seeds/scores.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return Promise.all([
-        knex('scores').insert({id: 10011, user_id: 10001, scores: 40, username: Edi}),
-        knex('scores').insert({id: 10012, user_id: 10002, scores: 30, username: Brian}),
-        knex('scores').insert({id: 10013, user_id: 10003, scores: 50, username: Mel}),
-        knex('scores').insert({id: 10014, user_id: 10004, scores: 20, username: Maia}),
-        knex('scores').insert({id: 10015, user_id: 10005, scores: 10, username: Guy}),
+        knex('scores').insert({id: 10011, user_id: 10001, scores: 40, username: 'Edi'}),
+        knex('scores').insert({id: 10012, user_id: 10002, scores: 30, username: 'Brian'}),
+        knex('scores').insert({id: 10013, user_id: 10003, scores: 50, username: 'Mel'}),
+        knex('scores').insert({id: 10014, user_id: 10004, scores: 20, username: 'Maia'}),
+        knex('scores').insert({id: 10015, user_id: 10005, scores: 10, username: 'Guy'}),
       ])
     })
 }
